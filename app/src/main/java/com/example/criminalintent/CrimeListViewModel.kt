60fb92f35861ab2b14ef95317f1132f52ddf70b8
dmarcha <dmarcha@cpp.edu>
2023-09-20 18:25:17 -0700

    added vcs file

---
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.criminalintent
+
+import androidx.lifecycle.ViewModel
+import java.util.Date
+import java.util.UUID
+
+class CrimeListViewModel : ViewModel() {
+    val crimes = mutableListOf<crime>()
+
+    init {
+        for (i in 0 until 100) {
+            val crime = crime(
+                id = UUID.randomUUID(),
+                title = "Crime #$i",
+                date = Date(),
+                isSolved = i % 2 ==0
+            )
+            crimes += crime
+        }
+    }
+}
