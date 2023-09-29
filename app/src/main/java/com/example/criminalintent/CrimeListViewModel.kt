a4014affa9d599291cc4661302bb0393bba31601
dmarcha <dmarcha@cpp.edu>
2023-09-29 10:59:10 -0700

    add coroutines

--- app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -1,22 +1,41 @@
 package com.example.criminalintent
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
 
-
+private const val TAG = "CrimeListViewModel"
 class CrimeListViewModel : ViewModel() {
     val crimes = mutableListOf<crime>()
 
     init {
+        Log.d(TAG, "init starting")
+        viewModelScope.launch {
+            Log.d(TAG, "coroutine launched")
+                crimes += loadCrimes()
+
+
+            Log.d(TAG,"Loading crimes finished")
+        }
+
+    }
+    suspend fun loadCrimes(): List<crime> {
+        val result = mutableListOf<crime>()
+        delay(5000)
         for (i in 0 until 100) {
             val crime = crime(
                 id = UUID.randomUUID(),
                 title = "Crime #$i",
                 date = Date(),
-                isSolved = i % 2 ==0
+                isSolved = i % 2 == 0
             )
-            crimes += crime
+            result += crime
         }
+        return result
     }
+
 }
